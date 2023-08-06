756a7a02cb5419525e58f3a8ec01a35b07cb28f5
Pranav Lonari <pranavlonari12@gmail.com>
2023-08-06 22:18:51 +0530

    Tuple example

---
+++ Tuple.py
@@ -0,0 +1,16 @@
+#empty tuple
+
+my_tuple = ()
+print(my_tuple)
+
+#Tuple having integer values
+my_tuple=(1,2,3)
+print("elements are :",my_tuple)
+
+#Tuple having mixed data 
+my_tuple=(1,2,3,"hello")
+print("elements are :",my_tuple)
+
+#nested tuple
+my_tuple=("abc",[2,4,6],(1,2,3))
+print("elements are :",my_tuple)
