fa80e0a478fbb42787e71f5302a2e58b627c71a1
Pranav Lonari <pranavlonari12@gmail.com>
2023-08-07 00:39:03 +0530

    iterating tuple added

--- Tuple.py
+++ Tuple.py
@@ -14,5 +14,25 @@ print("elements are :",my_tuple)
 #nested tuple
 my_tuple=("abc",[2,4,6],(1,2,3))
 print("elements are :",my_tuple)
-my_tuple += 4
-print(my_tuple) # will show TypeError: can only concatenate tuple (not "int") to tuple
+# my_tuple += 4
+# print(my_tuple) # will show TypeError: can only concatenate tuple (not "int") to tuple
+
+#accessing element by index
+print(my_tuple[0])
+
+#accessing by negative index
+
+print(my_tuple[-1])
+
+#accessing elements by slicing
+print(my_tuple[-1:])
+
+print(my_tuple.count('abc'))
+
+
+print(my_tuple.index('abc'))
+
+#iterating tuple
+
+for my_tuple in my_tuple:
+    print(my_tuple)
