e569d650dc20faa636f4594cd5ae750c0673e7f4
Pranav Lonari <pranavlonari12@gmail.com>
2023-08-06 16:04:39 +0530

    slicingexample

---
+++ Function.py
@@ -0,0 +1,7 @@
+import random
+print(random.randrange(10,20))
+list1=['a','b','c']
+print(random.choice(list1))
+random.shuffle(list1)
+print(list1)
+print(random.random())
---
+++ slicingexample.py
@@ -0,0 +1,9 @@
+list1=['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
+#from index 2 to 4
+print(list1[2:5])
+
+#from 5 to end
+print(list1[5:])
+
+#from beginning
+print(list1[:])
