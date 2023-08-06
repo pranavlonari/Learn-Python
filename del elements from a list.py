8044fbb8f2b9a6607068e2991a3e88c84e5f0b8f
Pranav Lonari <pranavlonari12@gmail.com>
2023-08-06 17:41:52 +0530

    del elements from a list

---
+++ del elements from a list.py
@@ -0,0 +1,14 @@
+lang = ['python','swift','c','c++','java']
+#deleting using del keyword
+del lang[1]
+#deleted index 1 element
+
+print(lang)
+
+#deleting element using remove
+lang.remove('python')
+
+#printing list after remove 
+print(lang)
+
+
