b200513b812bec151d5c13248700d79422dbaa4a
Pranav Lonari <pranavlonari12@gmail.com>
2023-08-06 17:22:40 +0530

    adding element using diffrent methods

--- element adding into list.py
+++ element adding into list.py
@@ -1,1 +1,19 @@
+list1=[10,12,14,16]
+print("before append:",list1)
+
+list1.append(30)
+print("after append:",list1)
+
+#appending list using extend
+
+even_num=[3,5,7]
+
+print("Before appending by extend :",list1)
+
+list1.extend(even_num)
+
+print("after appending using extend : ",list1)
+#sorting list by using sort method
+list1.sort()
+print("after sorting:",list1)
 
